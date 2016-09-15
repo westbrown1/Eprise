1a1c598b62059c4ce02822152160d87d1732a298
West Brown <lawsonsdad@gmail.com>
2016-09-14 23:22:16 -0400

    changed yml files back

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.1'
 
 set :application, 'accesstech'
-set :repo_url, 'git@github.com:westbrown1/eprise.git'
+set :repo_url, 'https://github.com/westbrown1/eprise'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

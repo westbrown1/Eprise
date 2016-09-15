ceeba3adc89111a8f4438c658388661d86fd5ed3
West Brown <lawsonsdad@gmail.com>
2016-09-14 22:22:21 -0400

    database.yml and secrets.yml to examples

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.1'
 
 set :application, 'accesstech'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:westbrown1/eprise.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,10 +24,10 @@ set :deploy_to, '/home/deploy/eprise'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
